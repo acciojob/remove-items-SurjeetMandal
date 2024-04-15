5322525de8ee5a8a4d14b374ef20530f9b7867fc
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-15 10:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+let deleteBtn = document.getElementById("deleteBtn");
+
+deleteBtn.addEventListener("click", function() {
+	let indexOfColorToDelete = select.selectedIndex;
+
+	if (indexOfColorToDelete !== -1) {
+		select.remove(indexOfColorToDelete);
+	}
+	else{
+		alert("please select a color to be deleted");
+	}
+})
